c0c79b4136d48571392a4cd0fddb41fe143a4859
petru-robu <petru.r.robu@gmail.com>
2023-03-20 19:43:49 +0200

    update

--- exe-downloader/main.py
+++ exe-downloader/main.py
@@ -1,29 +1,47 @@
-import requests
-import bs4
-import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.options import Options
 
-import urllib
+class ExeScraper:
+    def __init__(self):
+        options = Options()
+        options.add_argument("-profile")
+        options.add_argument('/home/petru/.mozilla/firefox/t1p5qnqs.Selenium Downloader')
+        firefox_capabilities = DesiredCapabilities.FIREFOX
+        firefox_capabilities['marionette'] = True
 
-#https://wildbit-soft.fi/download/dev/6_9/ViewerSetup.exe
+        self.driver = webdriver.Firefox(capabilities=firefox_capabilities, options=options)
 
+    def __del__(self):
+        self.driver.quit()
 
-url = 'https://wildbit-soft.fi/download/dev/6_9/ViewerSetup.exe'
+    def scrape_page_of_exe(self, url):
+        self.driver.get(url)
+        app_icons = self.driver.find_elements(By.CLASS_NAME, 'appicon')
+        for i in range(len(app_icons)):
+            self.driver.implicitly_wait(5)
+            app_icons[i].click()
 
-filename = 'download.exe'  
-urllib.request(url, filename)
+            self.driver.implicitly_wait(5)
+            dl_butt = self.driver.find_element(By.ID, 'sf-dlbutton')
+            dl_butt.click()
 
+            self.driver.implicitly_wait(5)
+            main_dl_butt = self.driver.find_element(By.CLASS_NAME, 'main-dlink')
+            main_dl_butt.click()
 
+            self.driver.implicitly_wait(5)
+            self.driver.back()
+            self.driver.back()
 
-'''
-browser = webdriver.Firefox()
+            app_icons = self.driver.find_elements(By.CLASS_NAME, 'appicon')
+    
+    def run(self):
+        base = 'https://www.snapfiles.com/new/list-whatsnew.html'
+        for i in range(2, 16):
+            url = base[:len(base)-5] + str(i) + '.html'
+            self.scrape_page_of_exe(url)
 
-browser.get('http://www.youtube.com')
-assert 'YouTube' in browser.title
-
-elem = browser.find_element(By.ID, 'search-input')  # Find the search box
-elem.send_keys('test' + Keys.RETURN)
-
-browser.quit()'''
+inst  = ExeScraper()
+inst.run()
